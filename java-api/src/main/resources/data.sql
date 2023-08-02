52e1aee38c045ee07347b7fcfb85b8ecc32f14de
domo-gonzalez <domingo.gonzalez001@umb.edu>
2023-08-02 14:30:41 -0400

    added more data

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,15 @@
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+insert into users(name, email, password) values (
+    'vansh', 'vansh@db.com', 'cheese'
+);
+
+-- insert into security(unit_price, coupon_percent, bond_currency, cusip, face_value_mn,isin,
+--                      issuer_name, bond_maturity_date, status, type)(
+--     select unit_price, coupon_percent, bond_currency, cusip, `face value (mn)`,isin,
+--             issuer_name, bond_maturity_date, status, type FROM total
+-- );
+
+
+-- insert into counter_party(bond_holder)(
+--     select bond_holder FROM total where rownum > 1
+);
+
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,12 +1,57 @@
-DROP DATABASE IF EXISTS bonds;
-CREATE DATABASE bonds;
-use bonds;
+DROP TABLE IF EXISTS book_user;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS trade;
+DROP TABLE IF EXISTS security;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS counter_party;
+DROP TABLE IF EXISTS dogs;
+
+create table total as select * from CSVREAD
+    ('/Users/DomingoGonzalez/db_workspace/coding_challenge/java-api/src/main/resources/other/db-bonds-data.csv');
 
 CREATE TABLE users(
     id long AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(250) NOT NULL,
     email VARCHAR(250) NOT NULL,
     password VARCHAR(250) NOT NULL
+);
+
+
+create TABLE security(
+     id long AUTO_INCREMENT PRIMARY KEY,
+     unit_price DOUBLE,
+     coupon_percent DOUBLE,
+-- might need constraints on coupon_percent
+     bond_currency VARCHAR(50),
+     cusip VARCHAR(250),
+     face_value_mn int,
+     isin VARCHAR(250),
+     issuer_name VARCHAR(250),
+     bond_maturity_date VARCHAR(250),
+     status VARCHAR(50),
+     type VARCHAR(50)
+);
+
+-- could possibly be bond_holder
+CREATE TABLE counter_party (
+       id long AUTO_INCREMENT PRIMARY KEY,
+       bond_holder VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE trade(
+      id long AUTO_INCREMENT PRIMARY KEY,
+      trade_type VARCHAR(50),
+      trade_currency VARCHAR(50),
+      quantity int,
+      trade_settlement_date VARCHAR(250),
+      trade_status VARCHAR(250),
+      trade_date VARCHAR(250),
+      security_id LONG,
+      counter_party_id LONG,
+      FOREIGN KEY (security_id) REFERENCES security(id),
+      FOREIGN KEY (counter_party_id) REFERENCES counter_party(id)
+-- unit price maybe here
+-- counter_party_id
 );
 
 CREATE TABLE book(
@@ -17,38 +62,6 @@ CREATE TABLE book(
     -- put trade id in here as foreign key
 );
 
-CREATE TABLE trade(
-    id long AUTO_INCREMENT PRIMARY KEY,
-    trade_type VARCHAR(50),
-    trade_currency VARCHAR(50),
-    quantity int,
-    trade_settlement_date VARCHAR(250),
-    trade_status VARCHAR(250),
-    trade_date VARCHAR(250),
-    security_id LONG,
-    counter_party_id LONG,
-    FOREIGN KEY (security_id) REFERENCES security(id),
-    FOREIGN KEY (counter_party_id) REFERENCES counter_party(id)
-    -- unit price maybe here
-    -- counter_party_id
-);
-
-create TABLE security(
-    id long AUTO_INCREMENT PRIMARY KEY,
-    unit_price DOUBLE,
-    coupon_percent DOUBLE,
-    -- might need constraints on coupon_percent
-    bond_currency VARCHAR(50),
-    cusip VARCHAR(250),
-    face_value_mn int,
-    isin VARCHAR(250),
-    issuer_name VARCHAR(250),
-    bond_maturity_date VARCHAR(250),
-    status VARCHAR(50),
-    type VARCHAR(50),
-    trade_id LONG,
-    FOREIGN KEY (trade_id) REFERENCES trade(id)
-);
 
 CREATE TABLE book_user (
    id long AUTO_INCREMENT PRIMARY KEY,
@@ -58,9 +71,5 @@ CREATE TABLE book_user (
    FOREIGN KEY (book_id) REFERENCES book(id)
 );
 
--- could possibly be bond_holder
-CREATE TABLE counter_party (
-    id long AUTO_INCREMENT PRIMARY KEY,
-    bond_holder VARCHAR(250) NOT NULL
-);
+
 
