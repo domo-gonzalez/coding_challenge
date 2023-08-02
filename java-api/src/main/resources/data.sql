eee2fd8a65082d44ec8345dd3e957c83619ef5fe
domo-gonzalez <domingo.gonzalez001@umb.edu>
2023-08-02 15:37:49 -0400

    added securities to data

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -2,14 +2,14 @@ insert into users(name, email, password) values (
     'vansh', 'vansh@db.com', 'cheese'
 );
 
--- insert into security(unit_price, coupon_percent, bond_currency, cusip, face_value_mn,isin,
---                      issuer_name, bond_maturity_date, status, type)(
---     select unit_price, coupon_percent, bond_currency, cusip, `face value (mn)`,isin,
---             issuer_name, bond_maturity_date, status, type FROM total
--- );
+insert into security(unit_price, coupon_percent, bond_currency, cusip,face_value_mn,isin,
+                     issuer_name, bond_maturity_date, status, type)(
+    select unit_price, coupon_percent, bond_currency, cusip, "face_value (mn)",isin,
+            issuer_name, bond_maturity_date, status, type FROM total
+);
 
 
--- insert into counter_party(bond_holder)(
---     select bond_holder FROM total where rownum > 1
--- );
+insert into counter_party(bond_holder)(
+    select bond_holder from total
+);
 
