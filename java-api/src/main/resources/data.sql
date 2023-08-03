d027b9b56a7978f76663cf3a22890ad71a013c07
Vansh B <vanshb75@gmai.com>
2023-08-03 11:04:42 -0400

    updated counter_party in trade table

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -25,11 +25,12 @@ INSERT into book(book_name)(
 --    trade_status, trade_date FROM total
 --);
 INSERT INTO trade (trade_type, trade_currency, quantity, unit_price, trade_settlement_date,
-trade_status, trade_date, book_name)
+trade_status, trade_date, book_name, counter_party)
 SELECT t.trade_type, t.trade_currency, t.quantity, t.unit_price, t.trade_settlement_date,
-t.trade_status, t.trade_date, b.book_name
+t.trade_status, t.trade_date, b.book_name, c.bond_holder
 FROM total t
-LEFT OUTER JOIN book b ON lower(t.book_name) = b.book_name;
+LEFT OUTER JOIN book b ON lower(t.book_name) = b.book_name
+LEFT OUTER JOIN counter_party c ON lower(t.bond_holder) = c.bond_holder;
 
 
 
