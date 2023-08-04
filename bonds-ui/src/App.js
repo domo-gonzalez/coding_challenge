443edcad3cf1b69334da959a1c3121662731cfcc
domo-gonzalez <domingo_gonzalez@icloud.com>
2023-08-04 16:16:51 -0400

    added home and all bonds link

--- bonds-ui/src/App.js
+++ bonds-ui/src/App.js
@@ -5,22 +5,32 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import BondDetail from './components/BondDetail';
 import AllBonds from './components/AllBonds';
+import { Routes, Route } from 'react-router';
+import Home from './components/Home';
 
 function App() {
   return (
-    <AllBonds/>
-    // <Navbar expand="lg" className="bg-body-tertiary">
-    //   <Container>
-    //     <Navbar.Brand href="#home">Home</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="me-auto">
-    //         <Nav.Link href="#home">All Bonds</Nav.Link>
-    //         <Nav.Link href="#link">Login</Nav.Link>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
+    <>
+    
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/home">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/allbonds">All Bonds</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+    <Routes>
+      <Route path="home" element={<Home/>} />
+      <Route path="allbonds" element={<AllBonds/>} />
+    </Routes>
+    </>
+
   );
 }
 
