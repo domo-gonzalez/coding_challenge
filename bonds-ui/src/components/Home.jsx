92fe08aa70b1eca5609a07f96766e5595fca9d04
unknown <christine.wan2001@gmail.com>
2023-08-07 14:44:48 +0100

    cards update

--- bonds-ui/src/components/Home.jsx
+++ bonds-ui/src/components/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useState, useEffect }from 'react'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <Row xs={1} md ={2} className="g-4">
+      {Array.from({length:4}).map((_,idx)=>(
+        <Col key = {idx}>
+          <Card>
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <Card.Text>some info about bonds</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+
+    </Row>
+  );
 }
 
 export default Home
